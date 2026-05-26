5379543755b1e95aecb581cb20babffed95e589b
Ngoc Ha <nguyenha1342006@gmail.com>
2026-05-26 16:16:18 +0700

    Update UIRender.js

--- assets/js/UIRender.js
+++ assets/js/UIRender.js
@@ -10,7 +10,9 @@ class UIRender{
             <div class="product-card">
 
                 <div class="product-image">
+                <a href="pages/products/ProductDetail.html?id=${p.id}">
                     <img src="${p.image}" alt="${p.name}">
+                </a>
                 </div>
 
                 <div class="product-info">
@@ -21,10 +23,7 @@ class UIRender{
                     </p>
 
                     <button class="product-btn" data-id="${p.id}">Add to cart</button>
-                    <a class="view-link"
-                        href="pages/products/ProductDetail.html?id=${p.id}">
-                        View
-                    </a>
+                    
                 </div>
 
             </div>
