def445d1a33fbf07550cdec9e7bf76155489ebe5
Ngoc Ha <nguyenha1342006@gmail.com>
2026-05-31 12:56:49 +0700

    Update pagination.js

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,33 +1,41 @@
 //pagination.js//
 class Pagination {
-    constructor(manager, perPage = 9) {
-        this.manager = manager;
+    constructor({
+        perPage = 9,
+        container,
+        paginationBox,
+        getData,
+        renderData
+    }) {
         this.perPage = perPage;
         this.currentPage = 1;
 
-        this.container = document.querySelector(".product-list");
-        this.paginationBox = document.querySelector(".pagination");
+        this.container = document.querySelector(container);
+        this.paginationBox = document.querySelector(paginationBox);
+
+        this.getData = getData;
+        this.renderData = renderData;
     }
 
     get totalPages() {
-    return Math.ceil(
-        this.manager.getVisibleProducts().length / this.perPage
-    );
-}
+        return Math.ceil(
+            this.getData().length / this.perPage
+        );
+    }
 
     getPaginatedProducts() {
         const start = (this.currentPage - 1) * this.perPage;
         const end = start + this.perPage;
-        const products = this.manager.getVisibleProducts();
-        return products.slice(start, end);
+
+        return this.getData().slice(start, end);
     }
 
-    renderProducts() {
-        const products = this.getPaginatedProducts();
+    renderItems() {
+        const data = this.getPaginatedProducts();
 
         if (!this.container) return;
 
-        UIRender.renderProductList(products, ".product-list");
+        this.renderData(data);
     }
 
     renderPagination() {
@@ -35,17 +43,35 @@ class Pagination {
 
         let html = "";
 
-        html += `<button ${this.currentPage === 1 ? "disabled" : ""} id="prevPage">Prev</button>`;
+        html += `
+            <button
+                id="prevPage"
+                ${this.currentPage === 1 ? "disabled" : ""}
+            >
+                Prev
+            </button>
+        `;
 
         for (let i = 1; i <= this.totalPages; i++) {
             html += `
-                <button class="page-btn ${i === this.currentPage ? "active" : ""}" data-page="${i}">
+                <button
+                    class="page-btn ${i === this.currentPage ? "active" : ""}"
+                    data-page="${i}"
+                >
                     ${i}
                 </button>
             `;
         }
 
-        html += `<button ${this.currentPage === this.totalPages ? "disabled" : ""} id="nextPage">Next</button>`;
+        html += `
+            <button
+                id="nextPage"
+                ${this.currentPage === this.totalPages ? "disabled" : ""}
+            >
+                Next
+            </button>
+        `;
+
 
         this.paginationBox.innerHTML = html;
 
@@ -53,30 +79,43 @@ class Pagination {
     }
 
     bindEvents() {
-        document.getElementById("prevPage")?.addEventListener("click", () => {
-            if (this.currentPage > 1) {
-                this.currentPage--;
-                this.update();
-            }
-        });
 
-        document.getElementById("nextPage")?.addEventListener("click", () => {
-            if (this.currentPage < this.totalPages) {
-                this.currentPage++;
-                this.update();
-            }
-        });
+        document
+            .getElementById("prevPage")
+            ?.addEventListener("click", () => {
 
-        document.querySelectorAll(".page-btn").forEach(btn => {
-            btn.addEventListener("click", (e) => {
-                this.currentPage = Number(e.target.dataset.page);
-                this.update();
+                if (this.currentPage > 1) {
+                    this.currentPage--;
+                    this.update();
+                }
             });
-        });
+
+        document
+            .getElementById("nextPage")
+            ?.addEventListener("click", () => {
+
+                if (this.currentPage < this.totalPages) {
+                    this.currentPage++;
+                    this.update();
+                }
+            });
+
+        document
+            .querySelectorAll(".page-btn")
+            .forEach(btn => {
+
+                btn.addEventListener("click", e => {
+
+                    this.currentPage =
+                        Number(e.target.dataset.page);
+
+                    this.update();
+                });
+            });
     }
 
     update() {
-        this.renderProducts();
+        this.renderItems();
         this.renderPagination();
     }
 }
